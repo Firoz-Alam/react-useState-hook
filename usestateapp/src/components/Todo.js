3b3a4a704136b752c3df2754e41f45eed93bb8b7
Firoz-Alam <51852342+Firoz-Alam@users.noreply.github.com>
2022-09-02 18:10:50 +0600

    using state with class components

---
+++ usestateapp/src/components/Todo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default class Todo extends React.Component {
+    state = {
+        todo:'',
+        warning: null,
+    };
+
+    handleInput  = (e) => {
+        const inputValue  = e.target.value;
+        const warning = inputValue.includes('.js') ? "You have to know about JavaScript":null;
+
+        this.setState({
+            todo: inputValue,
+            warning:warning,
+        })
+    }
+
+    render() {
+
+        const {todo,warning} = this.state;
+
+        return (
+            <div>
+                <p>{todo}</p>
+                <p>
+                    <textarea name='todo' value={todo} onChange = {this.handleInput}/>
+                </p>
+
+                <hr/>
+
+                <h2>{warning || 'Good Choice !'}</h2>
+            </div>
+        );
+    }
+}
